3ad6d0f25ef5b3f78d25e5ae7cfdf0df7a275ecd
DRATHAH <xavier@petersonfoundation.com>
2024-11-02 12:57:24 -0400

    Player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,19 @@ public class Player : MonoBehaviour
 {
     [Tooltip("How fast player moves")]
     public float speed = 10;
+    [Tooltip("How high player jumps")]
+    public float jumpForce = 10f;
     [Tooltip("Mouse sensitivity")]
     public float sensitivity = 2;
     public Transform mainCam;
+    public LayerMask groundLayer;
+    public float gravity = -9.81f;
 
     float sideInput;
     float forwardInput;
+    float yVelocity;
+    float gravityAcceleration;
+    float jumpSpeed;
 
     Vector2 appliedMouseDelta; // Where player is trying to face
     Vector2 currentMouseLook; // Where player is currently facing
@@ -42,13 +49,33 @@ public class Player : MonoBehaviour
             transform.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up); // Rotate player object to face camera direction
         }
 
+        // Jumping
+        gravityAcceleration = gravity * Time.deltaTime; // Calculate gravity acceleration
+        jumpSpeed = Mathf.Sqrt(jumpForce * -2 * gravity); // Calculate force at which player jumps, accounting for gravity
+        if (IsGrounded() && Input.GetKey(KeyCode.Space))
+        {
+            yVelocity = jumpSpeed; // Allow player to jump if grounded
+        }
+
+        yVelocity += gravityAcceleration; // Add gravity force to player
+
         // Movement
         sideInput = Input.GetAxis("Vertical");
         forwardInput = Input.GetAxis("Horizontal");
 
         Quaternion yaw = Quaternion.Euler(0, mainCam.eulerAngles.y, 0); // Take into account where player is facing
         Vector3 movement = yaw * new Vector3(forwardInput * speed, 0, sideInput * speed);
+        movement.y = yVelocity;
         movement *= Time.deltaTime;
         controller.Move(movement);
+
+    }
+
+    bool IsGrounded() // Check if player is on ground
+    {
+        Vector3 start = controller.transform.TransformPoint(controller.center); // get center of player
+        float rayLength = controller.height / 2 - controller.radius + 0.1f; // Distance from start to end of controller + bit extra to detect ground
+        bool hasHit = Physics.SphereCast(start, controller.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
     }
 }
