3e53077efd8a5981bb609a77fbfe548f4d0f7853
Guufla <liamabramov@gmail.com>
2024-11-02 21:49:33 -0400

    Quick change

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,28 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pickup : Interactable
 {
 
+    bool pickedUp = false;
+
+    Transform playerPickupPoint;
 
     // Start is called before the first frame update
     public override void StartEvents()
     {
         base.StartEvents();
+
+        playerPickupPoint = GameManager.instance.player.transform.GetChild(1).transform;
     }
 
     // Update is called once per frame
     public override void UpdateEvents()
     {
+        
         base.UpdateEvents();
+        if(pickedUp == true){
+            gameObject.transform.position = playerPickupPoint.position;
+            gameObject.transform.rotation = playerPickupPoint.rotation;
+        }
     }
 
     public void PickUp(){
+
         Debug.Log("Picked Up");
+        pickedUp = true;
     }
 
     public void Drop(){
         Debug.Log("Dropped");
+
+        pickedUp = false;
     }   
 }
