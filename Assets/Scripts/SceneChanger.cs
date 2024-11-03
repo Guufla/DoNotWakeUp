e2ba464ef8363c83dffdd02f3ff69a93ebbec53a
DRATHAH <xavier@petersonfoundation.com>
2024-11-03 13:44:26 -0500

    Scene changer

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeScene(int index)
     {
-        
+        StartCoroutine(LoadAsync(index));
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator LoadAsync(int sceneIndex)
     {
-        
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            // Progress text/slider value
+            yield return null;
+        }
     }
 }
