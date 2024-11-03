7e6c11ffa7823f554bc1c5abf61615a98f98251e
DRATHAH <xavier@petersonfoundation.com>
2024-11-03 14:11:07 -0500

    Transitions

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,73 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void ChangeScene(int index)
+    #region Singleton
+
+    public static SceneChanger instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    [Header("Transitions")]
+    public Transform slowFade;
+    public Transform flashFade;
+
+    Animator slowFadeAnimator;
+    Animator flashFadeAnimator;
+
+    bool isCalm = false;
+    bool isFlash = false;
+
+    int sceneIndex = 0;
+
+    private void Start()
+    {
+        slowFadeAnimator = slowFade.GetComponent<Animator>();
+        flashFadeAnimator = flashFade.GetComponent<Animator>();
+        flashFade.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isCalm)
+        {
+            AnimatorStateInfo info = slowFadeAnimator.GetCurrentAnimatorStateInfo(0);
+            if (info.IsName("FadeIn") && info.normalizedTime > 1)
+            {
+                isCalm = false;
+                ChangeScene(sceneIndex);
+            }
+        }
+
+        if (isFlash)
+        {
+            AnimatorStateInfo info = flashFadeAnimator.GetCurrentAnimatorStateInfo(0);
+            if (info.IsName("FlashIn") && info.normalizedTime > 1)
+            {
+                isFlash = false;
+                ChangeScene(sceneIndex);
+            }
+        }
+    }
+
+    public void CalmTransition(int index)
+    {
+        slowFadeAnimator.Play("FadeIn");
+        isCalm = true;
+        sceneIndex = index;
+    }
+    public void FlashTransition(int index)
+    {
+        flashFade.gameObject.SetActive(true);
+        flashFadeAnimator.Play("FlashIn");
+        isFlash = true;
+        sceneIndex = index;
+    }
+
+    void ChangeScene(int index)
     {
         StartCoroutine(LoadAsync(index));
     }
