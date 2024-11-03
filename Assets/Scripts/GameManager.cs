02775cfb9eeb2e7c65b7eebcceb464c086938a79
DRATHAH <xavier@petersonfoundation.com>
2024-11-03 12:14:19 -0500

    Clothes Minigame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -61,13 +61,14 @@ public class GameManager : MonoBehaviour
         // Once the death is forecasted they cannot lower 
         if (chillsSlider.value >= chillsSlider.maxValue && deathForcasted == false)
         {
-            deathTimer = Random.Range(1f, 30f);
             deathForcasted = true;
+            deathTimer = Random.Range(1f, 30f);
             Debug.Log(deathTimer);
         }
 
-        if (deathForcasted)
+        if (deathForcasted && !dead)
         {
+            dead = true;
             StartCoroutine(Death());
         }
     }
---
+++ Assets/Scripts/Minigames/Clothes.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Clothes : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public string borderTagName = "";
+    public string uiLayer = "Name of UI layer here";
+    public float throwForce = 100f;
+    public Rigidbody2D rb;
+    public float gravityScale = 10f;
+
+    bool canDrag = true;
+    Vector3 offset;
+    Vector3 mouseDelta = Vector3.zero;
+    Vector3 lastMousePos = Vector3.zero;
+
+    // Clothes functions
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        gameObject.layer = LayerMask.NameToLayer(uiLayer);
+        offset = transform.position - Input.mousePosition; // Gets position relative to where mouse clicked
+        rb.gravityScale = 0;
+        rb.velocity = Vector2.zero;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (canDrag)
+        {
+            mouseDelta = Input.mousePosition - lastMousePos;
+            lastMousePos = Input.mousePosition;
+
+            transform.position = Input.mousePosition + offset;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        rb.AddForceAtPosition(mouseDelta * throwForce, lastMousePos, ForceMode2D.Force);
+        rb.AddTorque(mouseDelta.magnitude * mouseDelta.normalized.x * throwForce, ForceMode2D.Force);
+        rb.gravityScale = gravityScale;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag(borderTagName))
+        {
+            canDrag = false;
+        }
+    }
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag(borderTagName))
+        {
+            canDrag = true;
+        }
+    }
+}
