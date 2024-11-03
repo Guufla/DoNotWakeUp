01087faaacdab9f0ebb7e9c92ae7dad3ee665836
DRATHAH <xavier@petersonfoundation.com>
2024-11-02 20:06:33 -0400

    Interact class finalized

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
+    public enum InteractableType
+    {
+        minigame,
+        interact,
+        pickup
+    }
+    public InteractableType type;
+
     public SphereCollider col;
     [Tooltip("Layer player is on")]
     public LayerMask playerLayer;
     [Tooltip("Actual interactable model")]
     public Transform interactable;
+    [Tooltip("Events to run when interacting")]
+    public UnityEvent interactEvent;
 
     bool hasPlayer = false;
     Transform player;
     GameObject promptCanvas;
     bool canInteract = false; // Allows player to start minigame
-    bool minigameStarted = false;
+    bool interacted = false;
 
     private void Start()
     {
@@ -46,7 +57,7 @@ public class Interactable : MonoBehaviour
             Vector3 direction = (interactable.position - player.position).normalized;
             Ray directRay = new Ray(player.position, direction);
             RaycastHit obstacle;
-            if (Physics.Raycast(directRay, out obstacle, col.radius * 2f, ~playerLayer) && !minigameStarted) // Make sure player is looking at interactable
+            if (Physics.Raycast(directRay, out obstacle, col.radius * 2f, ~playerLayer) && !interacted) // Make sure player is looking at interactable
             {
                 if (Vector3.Dot(player.transform.TransformDirection(transform.forward), direction) > 0.5f)
                 {
@@ -64,13 +75,37 @@ public class Interactable : MonoBehaviour
 
         if (canInteract && Input.GetKey(KeyCode.E))
         {
-            StartMinigame();
+            if (type == InteractableType.minigame)
+            {
+                StartMinigame();
+            }
+            else if (type == InteractableType.interact)
+            {
+                Interact();
+            }
+            else
+            {
+                Pickup();
+            }
+
+            interacted = true;
+            promptCanvas.SetActive(false);
         }
     }
 
     public virtual void StartMinigame()
     {
-        minigameStarted = true;
-        promptCanvas.SetActive(false);
+        Debug.Log("Started minigame");
+    }
+
+    public virtual void Interact()
+    {
+        interactEvent.Invoke();
+        Debug.Log("Interacted");
+    }
+
+    public virtual void Pickup()
+    {
+        Debug.Log("Interacted");
     }
 }
